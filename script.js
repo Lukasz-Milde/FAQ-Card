04fa260370e94b38a180cebd0a73a3b134acbec8
Lukasz-Milde <lukasz@milde.live>
2022-12-14 15:07:52 +0100

    mobile

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const quest = document.querySelectorAll(".accordion__item");
 
 quest.forEach((q) => {
 	q.addEventListener("click", () => {
-		console.log("click clik");
 		q.classList.toggle("active");
 	});
 });
